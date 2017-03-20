f3d0d4b3513ea2582994594c2f830529c11c5805
Hu Zheng <freetiger18@gmail.com>
2017-03-21 00:28:43 +0800

    Deal with more complex select queries.

--- src/main/scala/example/HelloWorld.scala
+++ src/main/scala/example/HelloWorld.scala
@@ -5,9 +5,17 @@ import info.axurez.scalaql.ScalaQL
 /**
   * Created by zhenghu on 17-3-14.
   */
+
+/**
+  * The hello-world-ish examples of the DSL.
+  */
 object HelloWorld extends ScalaQL {
   def main(args: Array[String]): Unit = {
     '> SELECT 'name FROM 'user WHERE 'haha END; // semicolon is compulsory. TODO: fix the postfix thing.
+    '> SELECT ('name, 'h) FROM 'user WHERE 'haha END; // TODO: It seems the Scala plugin on IntelliJ IDEA cannot parse this.
+    '> SELECT ('name, 'h, 'h1) FROM 'user WHERE 'haha END;
+    '> SELECT ('name, 'h, 'h1, 'h2) FROM 'user WHERE 'haha END;
+    '> SELECT * FROM 'user WHERE 'haha END; // semicolon is compulsory. TODO: fix the postfix thing.
     '> SELECT 'height FROM 'user END; // TODO: ponder on the combination/implicit conversion/type inference of 2 or more objects.
     '> SELECT 'age FROM; // TODO: detect such errors. Currently no error reported.
     //    'ha SELECT 'name
--- src/main/scala/info/axurez/scalaql/ScalaQL.scala
+++ src/main/scala/info/axurez/scalaql/ScalaQL.scala
@@ -4,8 +4,15 @@ import scala.collection.mutable
 /**
   * Created by zhenghu on 17-3-14.
   */
+
+/**
+  * The class.
+  */
 class ScalaQL {
-  
+
+  /**
+    * The base class for type matching.
+    */
   abstract sealed class BaseLine
   
   val queries: mutable.Queue[BaseLine] = mutable.Queue[BaseLine]()
@@ -18,15 +25,62 @@ class ScalaQL {
 
   abstract sealed class KEYWORD
 
+  sealed trait * {
+
+  }
+  object * extends * {
+
+  }
+
+  /**
+    * @todo Nothing is actually implemented since I don't master dependent typing.
+    * @param keyword The starting keyword.
+    */
+  case class QueryBuilder2(keyword: KEYWORD) {
+
+  }
+
+  /**
+    * The initial query builder class.
+    * Converted from the starting symbol/
+    * @param symbol The constructing symbol. Could only be `'>`, otherwise a runtime exception would be raised.
+    */
   case class QueryBuilder(symbol: Symbol) {
+
+    /**
+      *
+      */
     case object SELECT extends KEYWORD {
       def apply(columnName: Symbol): WithSelect = {
         val newSelect = WithSelect(columnName)
-//        queries.enqueue(newSelect)
         newSelect
+      }
+      def apply(sym: *): WithSelect = {
+        WithSelect('*)
+      }
+      def apply(columnNames: (Symbol, Symbol)): WithSelect = {
+        WithSelect('two)
+      }
+      def apply(columnNames: (Symbol, Symbol, Symbol)): WithSelect = {
+        WithSelect('three)
+      }
+      def apply(columnNames: (Symbol, Symbol, Symbol, Symbol)): WithSelect = {
+        WithSelect('four)
+      }
+
+      /**
+        * The apply method for 5 columns.
+        * @param columnNames The names of the 5 columns
+        * @return
+        */
+      def apply(columnNames: (Symbol, Symbol, Symbol, Symbol, Symbol)): WithSelect = {
+        WithSelect('five)
       }
     }
 
+    /**
+      *
+      */
     case object UPDATE extends KEYWORD {
       def apply(): UpdateQuery = {
         val newSelect = UpdateQuery()
@@ -67,6 +121,9 @@ class ScalaQL {
       }
     }
 
+    /**
+      *
+      */
     def END : Unit = {
       val newSelect = SelectQuery(columnName, tableName)
       queries.enqueue(newSelect)
@@ -80,8 +137,14 @@ class ScalaQL {
     }
   }
 
+  /**
+    *
+    */
   def GO() = execute()
 
+  /**
+    *
+    */
   private def execute() {
     while (queries.nonEmpty) {
       val query = queries.dequeue
@@ -95,10 +158,22 @@ class ScalaQL {
     }
   }
 
+  /**
+    *
+    * @param symbol The starting symbol.
+    * @return
+    */
   implicit def symbol2QueryBuilder(symbol: Symbol): QueryBuilder = symbol match {
     case '> => QueryBuilder(symbol)
     case _ => throw new Exception("Invalid")
   }
-  //implicit def Keyword2LineBuilder(keyword: KEYWORD) = QueryBuilder2()
+
+  /**
+    * Implicit conversion of `KEYWORD` to `QueryBuilder2`.
+    * Yet another query builder.
+    * @param keyword The keyword converted.
+    * @return
+    */
+  implicit def Keyword2LineBuilder(keyword: KEYWORD): QueryBuilder2 = QueryBuilder2(keyword) // It seems this is a dynamic way. TODO: probably could be static by dependent typing.
  // implicit def Line2LineBuilder(line: Int) = QueryBuilder()
 }
